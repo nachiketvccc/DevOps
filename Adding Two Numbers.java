62651bec0a6ccb01c00abf3024e530d2d0cd85e0
nachiketvccc <124669684+nachiketvccc@users.noreply.github.com>
2023-02-21 12:50:34 +0530

    Commit

--- Adding Two Numbers.java
+++ Adding Two Numbers.java
@@ -105,15 +105,3 @@ public class BasicCalculator {
 }
 
 Module - 3
-	
-	public class MultiplyExample {
-   public static void main(String[] args) {
-       int a;
-       int b;
-       int c;
-       a = 5;
-       b = 58;
-       c = a*b; //integer number to keep the result of multiplication
-       System.out.println("5*58 = " + c);
-   }
-}
