ff0294d95c22c0c71b870e2b4110909bbdcd1902
nachiketvccc <124669684+nachiketvccc@users.noreply.github.com>
2023-02-21 15:09:50 +0530

    Update

--- Adding Two Numbers.java
+++ Adding Two Numbers.java
@@ -1,15 +1,5 @@
 #  Adding of two numbers
-# This is java program
-# TRY TRY!!
-	# unwanted comments
-	# hello
-	# hello
-# python >>> java	
-# java code 
-Program list - Java
-	# Always give comment
-Module -1
-	Update
+# Module -1 
 
 import java.util.Scanner; // Import the Scanner class
 # This is java import statement
@@ -29,85 +19,4 @@ public class MyClass {
     }
 }
 
-Module -2 
- 
-// Java program for simple calculator
 
-import java.io.*;
-import java.lang.*;
-import java.lang.Math;
-import java.util.Scanner;
-public class BasicCalculator {
-
-	public static void main(String[] args)
-	{
-		// stores two numbers
-		double num1, num2;
-
-		// Take input from the user
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter the numbers");
-
-		// take the inputs
-		num1 = sc.nextDouble();
-
-		num2 = sc.nextDouble();
-
-		System.out.println("Enter the operator (+,-,*,/)");
-
-		char op = sc.next().charAt(0);
-
-		double o = 0;
-
-		switch (op) {
-
-		// case to add two numbers
-		case '+':
-
-			o = num1 + num2;
-
-			break;
-
-		// case to subtract two numbers
-		case '-':
-
-			o = num1 - num2;
-
-			break;
-
-		// case to multiply two numbers
-		case '*':
-
-			o = num1 * num2;
-
-			break;
-
-		// case to divide two numbers
-		case '/':
-
-			o = num1 / num2;
-
-			break;
-
-		default:
-
-			System.out.println("You enter wrong input");
-
-			break;
-		}
-
-		System.out.println("The final result:");
-
-		System.out.println();
-
-		// print the final result
-		System.out.println(num1 + " " + op + " " + num2
-						+ " = " + o);
-	}
-}
-
-Module - 3
-	Remove unwanted comments
-Module - 
-Module -5
